f1229e13cc6658404d1a9e0980b59314066a0e22
sarojregmi200 <sarojregmi.official@gmail.com>
2025-06-04 15:54:13 +0545

    feat: added workflow for publishing to npm

--- src/RangePicker/range-picker.tsx
+++ src/RangePicker/range-picker.tsx
@@ -6,8 +6,8 @@ import RangePickerHeader from "./Components/range-picker-header";
 import RangePickerInput from "./Components/range-picker-input";
 import { RangePickerProvider, useRangePicker } from "./hooks/useRangePicker";
 import { type tdirectionAwareContainerProps } from "../Components/helpers/direction-aware-container";
-import "../index.css";
 import { LocaleSwitcher } from "./Components/locale-switcher";
+import "../index.css";
 
 type tRangePickerWithoutInput = {
     /**
